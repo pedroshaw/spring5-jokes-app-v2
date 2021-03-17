0544f6c832853ed2a0f3948b1e3126af786702b0
pedroshaw <p_shaw@btconnect.com>
2021-03-17 15:56:06 +0000

    Changed including custom banner #7

--- src/main/java/guru/springframework/springbootjokesapp/controllers/JokeController.java
+++ src/main/java/guru/springframework/springbootjokesapp/controllers/JokeController.java
@@ -1,6 +1,7 @@
 package guru.springframework.springbootjokesapp.controllers;
 
 import guru.springframework.springbootjokesapp.services.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,12 +13,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class JokeController {
 
-    private JokeService jokeService;
+    private final JokeService jokeService;
 
+    @Autowired
     public JokeController(JokeService jokeService) {
         this.jokeService = jokeService;
     }
 
+    // The args allow a trailing slash or nothing at all
     @RequestMapping({"/",""})
     public String showJoke(Model model) {
 
