87ce60e7a1a735f94fc9f901e290ac4148d9a2b4
pedroshaw <p_shaw@btconnect.com>
2021-03-17 15:04:17 +0000

    Removed annotation

--- src/main/java/guru/springframework/springbootjokesapp/services/JokeService.java
+++ src/main/java/guru/springframework/springbootjokesapp/services/JokeService.java
@@ -1,12 +1,9 @@
 package guru.springframework.springbootjokesapp.services;
 
-import org.springframework.stereotype.Service;
-
 /**
  * User: p_sha
  * Date: Wed 17 Mar 2021
  */
-@Service
 public interface JokeService {
     String getJoke();
 }
